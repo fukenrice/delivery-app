fb7e21a9340469e5c358b2a4f75501aed9a13ef8
fukenrice <63079551+fukenrice@users.noreply.github.com>
2023-11-02 20:37:10 +0300

    Add networking dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
     implementation("com.google.dagger:dagger:2.46")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     kapt("com.google.dagger:dagger-compiler:2.46")
