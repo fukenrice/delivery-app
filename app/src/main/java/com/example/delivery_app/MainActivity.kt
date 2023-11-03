832476e7d7bc47ab498e1ff9d135d7d4d18fe68b
fukenrice <63079551+fukenrice@users.noreply.github.com>
2023-11-03 18:09:20 +0300

    Add action bar

--- app/src/main/java/com/example/delivery_app/MainActivity.kt
+++ app/src/main/java/com/example/delivery_app/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.delivery_app
 
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.delivery_app.databinding.ActionBarBinding
 import com.example.delivery_app.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,14 +25,23 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setCustomView(R.layout.action_bar)
+
+        val actionBarBinding = supportActionBar?.customView?.let { ActionBarBinding.bind(it) }
+
+        actionBarBinding?.llCity?.setOnClickListener {
+            // some onClick handler
+            Log.d(TAG, "City clicked")
+        }
+
+        actionBarBinding?.ivQr?.setOnClickListener {
+            // some onClick handler
+            Log.d(TAG, "QR clicked")
+        }
+
         navView.setupWithNavController(navController)
     }
 }
